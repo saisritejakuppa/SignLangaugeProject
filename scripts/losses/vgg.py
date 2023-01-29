cf40dd9efe2122db49733f47cc3d09ed885efd64
saisritejakuppa <saisritejakuppa@gmail.com>
2023-01-29 11:07:07 +0530

    vgg19

--- scripts/losses/vgg.py
+++ scripts/losses/vgg.py
@@ -127,8 +127,8 @@ class VGG19(torch.nn.Module):
         super(VGG19, self).__init__()
         # features = models.vgg19(pretrained=True).features
 
-        vgg19 = models.vgg19(pretrained=False)
-        vgg19.load_state_dict(torch.load('/home/haihuam/CASD-main/dataset/fashion/vgg19-dcbb9e9d.pth'))
+        vgg19 = models.vgg19(pretrained=True)
+        # vgg19.load_state_dict(torch.load('/home/haihuam/CASD-main/dataset/fashion/vgg19-dcbb9e9d.pth'))
         self.vgg = vgg19.features
         features = vgg19.features
 
